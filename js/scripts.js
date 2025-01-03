52202b4babcfa2308f117931c5be76ac82f4ade6
sm00chie <112527118+sm00chie@users.noreply.github.com>
2025-01-02 20:58:22 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+function toggleNav() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('open');
+}
+
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('open');
     this.classList.toggle('open');
