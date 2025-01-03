0fab42fba3016928c29fb219cdf4ac4afde533fe
sm00chie <112527118+sm00chie@users.noreply.github.com>
2025-01-03 15:32:41 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-function toggleNav() {
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('open');
-}
-
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('open');
     this.classList.toggle('open');
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/nav.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('nav-placeholder').innerHTML = html;
+        })
+        .catch(error => console.error('Error loading navigation:', error));
+});
+
+function toggleNav() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('open');
+}
